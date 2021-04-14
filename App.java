b182b5c49cc9ffb9502ec7ab08bee23288a1bfe2
tonchez1019 <64612578+tonchez1019@users.noreply.github.com>
2021-04-14 13:39:51 -0500

    cambio
    se genera un pequeño cambio en la actividad

--- App.java
+++ App.java
@@ -1,18 +1,45 @@
 import java.util.Random;
+import java.util.Scanner;
 
-public class App {
+public class App2 {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         
-        
+        int respuesta=1;
         Deck deckP = new Deck();
-        deckP.cards();
-        System.out.println(deckP.toString());
-        deckP.shuffle();
-        System.out.println(deckP.toString());
-        deckP.head();
-        deckP.pick();
-        deckP.hand();
+     
 
 
+        System.out.println("Bienvenido a Poker");
+        
+        while(respuesta != 0){
+            
+        System.out.println("Selecciona una opcion" + "\n" + "1- Mezclar deck \n 2- Sacar Carta \n 3- Carta al alzar \n 4- Generar una mano de 5 cartas \n 0- Salir");
+        respuesta = sc.nextInt();
+
+        switch (respuesta) 
+        { 
+            case 1: 
+                deckP.shuffle();
+                     break;
+            case 2:  
+                deckP.head();
+                     break;
+            case 3:  
+                deckP.pick();
+                     break;
+            case 4:  
+                deckP.hand();
+                     break;
+            case 0: 
+                System.out.println("Gracias por jugar ");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+            }
+        }
     }
 }
